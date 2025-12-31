dde73f1ed7c7ec0aacc188a3473e6ccd8e968cf2
nguyenthihuyen <nguyenthihuyen@cityascom.vn>
2025-12-31 08:56:35 +0700

    Add ios folder with Podfile and xcodeproj, update App.js

--- App.js
+++ App.js
@@ -3,8 +3,10 @@ import { SafeAreaView, Text } from 'react-native';
 
 export default function App() {
   return (
-    <SafeAreaView style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Hello Huyền! Wellcome to iosCloudApp</Text>
-    </SafeAreaView>
+	<SafeAreaProvider>
+    		<SafeAreaView style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      			<Text>Hello Huyền! Wellcome to iosCloudApp</Text>
+    		</SafeAreaView>
+ 	</SafeAreaProvider>
   );
 }
